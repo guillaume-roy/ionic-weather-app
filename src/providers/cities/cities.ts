d4231cc60a9c51852161d14605df12e683f6c2b4
Guillaume ROY <gr.guillaume.roy@gmail.com>
2018-10-16 17:34:04 +0200

    Create basic methods for CitiesProvider

--- src/providers/cities/cities.ts
+++ src/providers/cities/cities.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ICity } from '../../models/city';
 
 /*
   Generated class for the CitiesProvider provider.
@@ -12,5 +13,21 @@ export class CitiesProvider {
 
   constructor(public http: HttpClient) {
     console.log('Hello CitiesProvider Provider');
+  }
+
+  search(query: string): Promise<ICity[]> {
+    return Promise.resolve([]);
+  }
+
+  getAllFromBookmarked(): Promise<ICity[]> {
+    return Promise.resolve([]);
+  }
+
+  deleteInBookmark(city: ICity): Promise<void> {
+    return Promise.resolve();
+  }
+
+  saveInBookmark(city: ICity): Promise<void> {
+    return Promise.resolve();
   }
 }
